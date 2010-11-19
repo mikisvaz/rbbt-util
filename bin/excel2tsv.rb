f775f14d894aa9780abec7b27e59638c276c6593
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2010-11-19 12:55:17 +0100

    Excel to tsv

---
+++ bin/excel2tsv.rb
@@ -0,0 +1,28 @@
+require 'spreadsheet'
+require 'simpleopt'
+
+options = SOPT.get("-h--header:-o--outfile*:-i--infile*:-p--page*")
+
+infile  = options[:infile] || "-"
+outfile = options[:outfile] || "-"
+
+page    = options[:page]   || 1
+page = page.to_i - 1
+
+i = (infile == '-' ? STDIN : File.open(infile))
+
+workbook = Spreadsheet.open i
+sheet    = workbook.worksheet 0
+
+rows = []
+
+sheet.each do |row|
+  rows << row.values_at(0..(row.size - 1))
+end
+
+if options[:header]
+  header = rows.shift
+  puts "#" + header * "\t"
+end
+
+rows.each do |row| puts row * "\t" end
