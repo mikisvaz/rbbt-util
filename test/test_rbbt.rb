2903dc3e37fdc81660ebe599a19e959d37db1227
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2010-12-02 20:59:33 +0100

    nil

--- test/test_rbbt.rb
+++ test/test_rbbt.rb
@@ -3,7 +3,7 @@ require 'rbbt'
 require 'rbbt/util/misc'
 
 class TestBase < Test::Unit::TestCase
-  def test_url
+  def _test_url
     Rbbt.add_datafiles :test => ['test', 'http://google.com']
     assert(Misc.fixutf8(File.open(Rbbt.find_datafile('test')).read) =~ /html/)
     FileUtils.rm Rbbt.find_datafile('test')
