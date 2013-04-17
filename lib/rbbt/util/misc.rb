2565a583684bc12f97935fe9d285cc44a9a0af78
Miguel Vazquez <miguel.vazquez@cnio.es>
2013-04-17 12:27:05 +0200

    nil

--- lib/rbbt/util/misc.rb
+++ lib/rbbt/util/misc.rb
@@ -165,6 +165,8 @@ module Misc
 
   def self.remove_long_items(obj)
     case
+    when TSV === obj
+      remove_long_items(obj.fields + obj.keys.sort)
     when (Array === obj and obj.length > ARRAY_MAX_LENGTH)
       remove_long_items(obj[0..ARRAY_MAX_LENGTH-2] << "TRUNCATED at #{ ARRAY_MAX_LENGTH } (#{obj.length})")
     when (Hash === obj and obj.length > ARRAY_MAX_LENGTH)
