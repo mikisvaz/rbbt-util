9cb93b1640f630dbee63912b0231731190277e20
Miguel Vazquez <miguel.vazquez@cnio.es>
2013-10-29 16:36:41 +0100

    nil

--- lib/rbbt/association/item.rb
+++ lib/rbbt/association/item.rb
@@ -42,4 +42,28 @@ module AssociationItem
       Hash[*fields.zip(v).flatten]
     }
   end
+
+  def self.incidence(pairs)
+    matrix = {}
+    targets = []
+    sources = []
+    matches = {}
+
+    pairs.each do |p|
+      s, sep, t = p.partition "~"
+      sources << s
+      targets << t
+      matches[s] ||= Hash.new{false}
+      matches[s][t] = true
+    end
+
+    sources.uniq!
+    targets = targets.uniq.sort
+
+    matches.each do |s,hash|
+      matrix[s] = hash.values_at(*targets)
+    end
+
+    defined?(TSV)? TSV.setup(matrix, :fields => targets, :type => :list) : matrix
+  end
 end
