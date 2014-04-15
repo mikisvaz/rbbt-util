5503cf6cd626435f24cd7a1ff7b9ae72cd7f902c
Miguel Vazquez <miguel.vazquez@cnio.es>
2014-04-15 13:08:54 +0200

    nil

--- lib/rbbt/workflow/accessor.rb
+++ lib/rbbt/workflow/accessor.rb
@@ -49,8 +49,8 @@ class Step
         end
 
         begin
-          @info_cache = Misc.insist(2, 2, info_file) do
-            Misc.insist(2, 0.5, info_file) do
+          @info_cache = Misc.insist(2, 3, info_file) do
+            Misc.insist(2, 1, info_file) do
               Misc.insist(3, 0.2, info_file) do
                 Open.open(info_file) do |file|
                   INFO_SERIALIAZER.load(file) || {}
