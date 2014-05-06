a409a7bdb454eef01804cafb7dcf999aba567270
Miguel Vazquez <miguel.vazquez@cnio.es>
2014-05-06 13:28:23 +0200

    nil

--- lib/rbbt/workflow.rb
+++ lib/rbbt/workflow.rb
@@ -236,7 +236,7 @@ module Workflow
   def get_job_step(step_path, task = nil, input_values = nil, dependencies = nil)
     step_path = step_path.call if Proc === step_path
     persist = input_values.nil? ? false : true
-    #persist = false
+    persist = false
     key = Path === step_path ? step_path.find : step_path
     step = Persist.memory("Step", :key => key, :repo => step_cache, :persist => persist) do
       step = Step.new step_path, task, input_values, dependencies
