fc96b2293b7a44151a016fba2f1c42c620443eba
Miguel Vazquez <miguel.vazquez@cnio.es>
2014-10-07 17:02:46 +0200

    nil

--- etc/app.d/finder.rb
+++ etc/app.d/finder.rb
@@ -1,9 +1,9 @@
 #{{{ FINDER
 finder = Finder.new
-if ENV['RBBT_FINDER']
+#if ENV['RBBT_FINDER']
   finder.add_instance(KEGG.pathways, :grep => '^hsa\|^#', :fields => ["Pathway Name"], :namespace => Organism.default_code("Hsa"), :fix => Proc.new{|l| l.sub(/ - Homo sapiens.*/,'')})
   finder.add_instance(Organism.lexicon(Organism.default_code("Hsa")), :persist => true, :namespace => Organism.default_code("Hsa"), :grep => '^LRG_', :invert_grep => true)
-end
+#end
 set :finder, finder
 Log.debug("Finder started with: #{finder.instances.length} instances")
 
