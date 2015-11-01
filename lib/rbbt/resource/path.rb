6997c6a0baf5061ca3ef095394a8cf1c48ea9f50
Miguel Vazquez <miguel.vazquez@cnio.es>
2015-11-01 13:55:38 +0100

    nil

--- lib/rbbt/resource/path.rb
+++ lib/rbbt/resource/path.rb
@@ -5,6 +5,7 @@ module Path
   attr_accessor :resource, :pkgdir, :search_paths
 
   def self.setup(string, pkgdir = nil, resource = nil, search_paths = nil)
+    return string if string.nil?
     string = string.dup if string.frozen?
     string.extend Path
     string.pkgdir = pkgdir || 'rbbt'
