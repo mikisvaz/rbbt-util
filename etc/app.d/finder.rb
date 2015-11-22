f2c681b9f992875a2d16f791365f082bcc3adb53
Miguel Vazquez <miguel.vazquez@cnio.es>
2015-11-22 20:12:16 +0100

    Consider proteins in finder

--- etc/app.d/finder.rb
+++ etc/app.d/finder.rb
@@ -11,6 +11,10 @@ if ENV['RBBT_FINDER']
   mi_hash = {"Mutated Isoform" => /ENSP\w+:.+/}
   finder.add_instance(mi_hash, :namespace => organism) 
 
+  prot_hash = {"Ensembl Protein ID" => /ENSP\w+$/}
+  finder.add_instance(prot_hash, :namespace => organism) 
+
+
   organism_hash = {"organism" => /[A-Z][a-z]{2}(?:\/[a-z]{3}20\d\d)?/}
   finder.add_instance(organism_hash, :namespace => organism) 
 end
