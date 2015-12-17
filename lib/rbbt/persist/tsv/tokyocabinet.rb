22c388c0794cfad82765061475bd6c8a7078787d
Miguel Vazquez <miguel.vazquez@cnio.es>
2015-12-18 00:08:31 +0100

    nil

--- lib/rbbt/persist/tsv/tokyocabinet.rb
+++ lib/rbbt/persist/tsv/tokyocabinet.rb
@@ -8,10 +8,21 @@ module Persist
     attr_accessor :tokyocabinet_class
 
     def self.open(path, write, serializer, tokyocabinet_class = TokyoCabinet::HDB)
+      if String === tokyocabinet_class and tokyocabinet_class.include? ":big" 
+        big = true
+        tokyocabinet_class = tokyocabinet_class.split(":").first
+      else
+        big = false
+      end
+
       tokyocabinet_class = TokyoCabinet::HDB if tokyocabinet_class == "HDB" or tokyocabinet_class.nil?
       tokyocabinet_class = TokyoCabinet::BDB if tokyocabinet_class == "BDB"
 
       database = CONNECTIONS[path] ||= tokyocabinet_class.new
+
+      if big and not Open.exists? path
+        database.tune(nil,nil,nil,tokyocabinet_class::TLARGE | tokyocabinet_class::TDEFLATE) 
+      end
 
       flags = (write ? tokyocabinet_class::OWRITER | tokyocabinet_class::OCREAT : tokyocabinet_class::OREADER)
       database.close 
@@ -20,6 +31,7 @@ module Persist
         ecode = database.ecode
         raise "Open error: #{database.errmsg(ecode)}. Trying to open file #{path}"
       end
+
 
       database.extend Persist::TCAdapter unless Persist::TCAdapter === database
       database.persistence_path ||= path
