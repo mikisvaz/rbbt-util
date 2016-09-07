bd203099d86270ba4a4b00a0afb37eeae26e48ee
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2016-09-07 09:12:36 +0200

    nil

--- etc/app.d/finder.rb
+++ etc/app.d/finder.rb
@@ -2,7 +2,6 @@
 finder = Finder.new
 if ENV['RBBT_FINDER']
   organism = Organism.default_code("Hsa")
-  finder.add_instance(KEGG.pathways, :grep => '^hsa\|^#', :fields => ["Pathway Name"], :namespace => organism, :fix => Proc.new{|l| l.sub(/ - Homo sapiens.*/,'')}) if defined? KEGG
   finder.add_instance(Organism.lexicon(organism), :persist => true, :namespace => organism, :grep => Organism.blacklist_genes(organism).list, :invert_grep => true) if defined? Organism
 
   mutation_hash = {"Genomic Mutation" => /\w+:\d+:[ACTG\-\+]+/}
