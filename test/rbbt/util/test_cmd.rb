14947cc7ebcab5da440d09918e2f642ae1b133b9
Miguel Vazquez <miguel.vazquez.g@bsc.es>
2018-01-24 10:56:31 +0100

    CMD test fixes

--- test/rbbt/util/test_cmd.rb
+++ test/rbbt/util/test_cmd.rb
@@ -5,27 +5,27 @@ require 'test/unit'
 class TestCmd < Test::Unit::TestCase
 
   def test_cmd_option_string
-    assert_equal("--user-agent firefox", CMD.process_cmd_options("--user-agent" => "firefox"))
-    assert_equal("--user-agent=firefox", CMD.process_cmd_options("--user-agent=" => "firefox"))
+    assert_equal("--user-agent 'firefox'", CMD.process_cmd_options("--user-agent" => "firefox"))
+    assert_equal("--user-agent='firefox'", CMD.process_cmd_options("--user-agent=" => "firefox"))
     assert_equal("-q", CMD.process_cmd_options("-q" => true))
     assert_equal("", CMD.process_cmd_options("-q" => nil))
     assert_equal("", CMD.process_cmd_options("-q" => false))
 
-    assert(CMD.process_cmd_options("--user-agent" => "firefox", "-q" => true) =~ /--user-agent firefox/)
+    assert(CMD.process_cmd_options("--user-agent" => "firefox", "-q" => true) =~ /--user-agent 'firefox'/)
     assert(CMD.process_cmd_options("--user-agent" => "firefox", "-q" => true) =~ /-q/)
   end
 
   def test_cmd
     assert_equal("test\n", CMD.cmd("echo '{opt}' test").read)
     assert_equal("test", CMD.cmd("echo '{opt}' test", "-n" => true).read)
-    assert_equal("test2\n", CMD.cmd("cut", "-f" => 2, "-d" => '" "', :in => "test1 test2").read)
+    assert_equal("test2\n", CMD.cmd("cut", "-f" => 2, "-d" => ' ', :in => "test1 test2").read)
   end
 
   def test_pipe
     assert_equal("test\n", CMD.cmd("echo test", :pipe => true).read)
     assert_equal("test\n", CMD.cmd("echo '{opt}' test", :pipe => true).read)
     assert_equal("test", CMD.cmd("echo '{opt}' test", "-n" => true, :pipe => true).read)
-    assert_equal("test2\n", CMD.cmd("cut", "-f" => 2, "-d" => '" "', :in => "test1 test2", :pipe => true).read)
+    assert_equal("test2\n", CMD.cmd("cut", "-f" => 2, "-d" => ' ', :in => "test1 test2", :pipe => true).read)
   end
 
   def test_error
