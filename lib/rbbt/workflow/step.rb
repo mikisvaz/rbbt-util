3d2e325cf597e79d2d075ac869366b14ed00cf61
Miguel Vazquez <miguel.vazquez@ntnu.no>
2018-11-25 13:33:31 +0100

    nil

--- lib/rbbt/workflow/step.rb
+++ lib/rbbt/workflow/step.rb
@@ -58,7 +58,7 @@ class Step
     @info_mutex = Mutex.new
     @inputs = inputs 
     NamedArray.setup @inputs, task.inputs.collect{|s| s.to_s} if task and task.respond_to? :inputs and task.inputs
-    if File.exists?(info_file) and info[:path] != path
+    if Open.exists?(info_file) and (info[:path] != path)
       @relocated = true
     end
   end
