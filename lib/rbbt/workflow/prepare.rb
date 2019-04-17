e245d997aed05462ea4a1a0d3f1adf6c09df15e0
Miguel Vazquez <miguel.vazquez.g@bsc.es>
2019-04-18 00:35:39 +0200

    Prepare dependencies functionality

---
+++ lib/rbbt/workflow/prepare.rb
@@ -0,0 +1,23 @@
+class Step
+  def self.prepare_dependencies(jobs, tasks, cpus)
+    deps = []
+
+    jobs = [jobs] unless Array === jobs
+    tasks = [tasks] unless Array === tasks
+    tasks = tasks.collect{|t| t.to_s}
+
+    jobs.each do |job|
+      job.rec_dependencies.each do |dep|
+        next if dep.done?
+        deps << dep if tasks.include?(dep.task_name.to_s) or tasks.include?([dep.workflow.to_s, dep.task_name] * "#")
+      end
+    end
+
+    cpus = jobs.length if cpus.to_s == "max"
+    cpus = cpus.to_i if String === cpus
+    TSV.traverse deps.collect{|dep| dep.path}, :type => :array, :cpus => cpus, :bar => "Prepare dependencies #{Misc.fingerprint tasks} for #{Misc.fingerprint job}" do |path|
+      dep = deps.select{|dep| dep.path == path}.first
+      dep.produce
+    end
+  end
+end
