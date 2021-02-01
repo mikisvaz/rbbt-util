8062968925b906a6454b54c73fa8b18fa12ae932
Miguel Vazquez <miguel.vazquez.g@bsc.es>
2021-02-01 12:33:26 +0100

    nil

--- lib/rbbt/hpc/slurm.rb
+++ lib/rbbt/hpc/slurm.rb
@@ -255,7 +255,7 @@ EOF
 EOF
       annotate_cmd =<<-EOF
 #{exec_cmd} \\
-workflow write_info --recursive --force=false "$step_path" slurm_job $SLURM_JOB_ID 
+workflow write_info --recursive --force=false --check_pid "$step_path" slurm_job $SLURM_JOB_ID 
 EOF
 
 
