9a0d4e25cf232aead05ae4d6728d51946244a382
Miguel Vazquez <miguel.vazquez.g@bsc.es>
2022-02-11 16:13:05 +0100

    Test hash loading and saving of annotations

--- test/rbbt/annotations/test_util.rb
+++ test/rbbt/annotations/test_util.rb
@@ -1,6 +1,11 @@
 require File.join(File.expand_path(File.dirname(__FILE__)), '../..', 'test_helper.rb')
 require 'rbbt/annotations'
 
+module TestEntityString
+  extend Entity
+  self.annotation :code
+end
+
 class TestAnnotation < Test::Unit::TestCase
   def test_marshal
     a = "STRING"
@@ -8,6 +13,12 @@ class TestAnnotation < Test::Unit::TestCase
 
     assert !(Annotated === Marshal.load(Marshal.dump(a)))
     assert_equal a, Marshal.load(Marshal.dump(a))
+  end
+
+  def test_hash
+    e = TestEntityString.setup("TEST", :code => 10)
+    assert_equal "TEST", Annotated.to_hash(e)[:literal]
+    assert_equal 10, Annotated.to_hash(e)[:info][:code]
   end
 end
 
