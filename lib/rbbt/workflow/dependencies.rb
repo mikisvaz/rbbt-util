64c777d201082fc4f9d28f8496808f39462907cd
Miguel Vazquez <miguel.vazquez.g@bsc.es>
2022-11-30 12:31:05 +0100

    Fixed a but introduced in overriding dependencies

--- lib/rbbt/workflow/dependencies.rb
+++ lib/rbbt/workflow/dependencies.rb
@@ -72,7 +72,7 @@ module Workflow
   def setup_override_dependency(dep, workflow, task_name)
     return [] if dep == :skip || dep == 'skip'
 
-    if ! Step === dep
+    if not Step === dep
       located = Open.exists?(dep)
       dep = Workflow.load_step(dep)
     end
