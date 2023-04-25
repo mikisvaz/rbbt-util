54590dede56e6eca2c59d3a9026bf7408b078f31
Miguel Vazquez <mikisvaz@gmail.com>
2023-04-25 12:08:19 +0200

    nil

--- lib/rbbt/util/misc/inspect.rb
+++ lib/rbbt/util/misc/inspect.rb
@@ -331,7 +331,7 @@ module Misc
                 obj = (obj.resource || Rbbt).identify obj if RBBT_IDENTIFY_PATH
                 if obj.directory?
                   files = obj.glob("**/*")
-                  "directory: #{Misc.fingerprint(files)}"
+                  "directory: #{Misc.fingerprint(files.collect{|f| Misc.path_relative_to(obj.find, f)})}"
                 elsif obj.located?
                   "file: " << Open.realpath(obj) << "--" << mtime_str(obj)
                 else
