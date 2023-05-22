55e608d42dd5fb55232af329527991e46b7a0db5
Miguel Vazquez <mikisvaz@gmail.com>
2023-05-22 17:48:21 +0200

    Don't change default directories from scout-gear. Relay consume_stream

--- lib/rbbt-util.rb
+++ lib/rbbt-util.rb
@@ -1,17 +1,17 @@
 require_relative 'rbbt'
 require_relative 'rbbt/util/misc'
 require 'scout/open'
-Open.remote_cache_dir        = Rbbt.var.cache["open-remote"].find :user
+#Open.remote_cache_dir        = Rbbt.var.cache["open-remote"].find :user
 
 require 'set'
 require 'scout/persist'
-Persist.cache_dir = Rbbt.var.cache.persistence
+#Persist.cache_dir = Rbbt.var.cache.persistence
 
 require_relative 'rbbt/util/filecache'
-FileCache.cachedir   = Rbbt.var.cache.filecache.find :user
+#FileCache.cachedir   = Rbbt.var.cache.filecache.find :user
 
 require_relative 'rbbt/util/tmpfile'
-TmpFile.tmpdir       = Rbbt.tmp.find :user
+#TmpFile.tmpdir       = Rbbt.tmp.find :user
 
 require_relative 'rbbt/util/cmd'
 require_relative 'rbbt/tsv'
--- lib/rbbt/util/misc/refactor.rb
+++ lib/rbbt/util/misc/refactor.rb
@@ -5,6 +5,7 @@ require_relative '../log/refactor'
 
 Rbbt.relay_module_method Misc, :lock, Open, :lock
 Rbbt.relay_module_method Misc, :sensiblewrite, Open, :sensible_write
+Rbbt.relay_module_method Misc, :consume_stream, Open, :consume_stream
 Rbbt.relay_module_method Misc, :sort_stream, Open, :sort_stream
 Rbbt.relay_module_method Misc, :sanitize_filename, Path, :sanitize_filename
 Rbbt.relay_module_method Misc, :collapse_stream, Open, :collapse_stream
