b7e8d3554a05a73828b7bc2468720895880bcd08
Miguel Vazquez <miguel.vazquez.g@bsc.es>
2023-06-27 18:43:54 +0200

    Remove configuration of Rbbt resource in favour for Scout

--- lib/rbbt.rb
+++ lib/rbbt.rb
@@ -3,11 +3,5 @@ $LOAD_PATH.unshift File.join(__dir__, '../../lib')
 require 'scout/path'
 require 'scout/log'
 require_relative 'rbbt/resource'
-
-module Rbbt
-  extend Resource
-  self.pkgdir = 'rbbt'
-end
-
 require_relative 'rbbt/util/version'
 require_relative 'rbbt/refactor'
