3fb9340f7fcc5eadd31b3dad087b5032e9195233
Miguel Vazquez <miguel.vazquez.g@bsc.es>
2024-02-05 17:17:31 +0100

    Make a test more robust

--- test/rbbt/resource/test_path.rb
+++ test/rbbt/resource/test_path.rb
@@ -38,8 +38,8 @@ class TestTSV < Test::Unit::TestCase
     assert_equal Path.setup(File.join('doc', path)).find(:lib), path.find(:lib).doc_file
 
     assert_equal "lib/rbbt/resource.rb", path.doc_file.source_for_doc_file
-    assert_equal path.find, path.doc_file.find(:lib).source_for_doc_file
-    assert_equal path.find, path.doc_file.source_for_doc_file.find
+    assert_equal File.realpath(path.find), File.realpath(path.doc_file.find(:lib).source_for_doc_file)
+    assert_equal File.realpath(path.find), File.realpath(path.doc_file.source_for_doc_file.find)
 
     assert_equal "doc/lib/rbbt/resource.rb.doc", path.doc_file.set_extension('doc')
     #assert_equal "lib/rbbt/resource.rb", path.doc_file.set_extension('doc').source_for_doc_file
