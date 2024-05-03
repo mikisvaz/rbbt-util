47461a0b2897f27a36c685dbc756e3db739b378c
Miguel Vazquez <miguel.vazquez.g@bsc.es>
2024-05-03 11:56:11 +0300

    Move workflow install refactor

--- lib/rbbt-util.rb
+++ lib/rbbt-util.rb
@@ -25,32 +25,3 @@ Persist.cache_dir     = Rbbt.var.cache.persistence
 FileCache.cachedir    = Rbbt.var.cache.filecache.find :user
 TmpFile.tmpdir        = Rbbt.tmp.find :user
 Resource.default_resource = Rbbt
-
-
-class << Workflow
-  def workflow_dir
-    @workflow_dir || 
-      ENV["RBBT_WORKFLOW_DIR"] || 
-      begin 
-        workflow_dir_config = Path.setup("etc/workflow_dir")
-        if workflow_dir_config.exists?
-          Path.setup(workflow_dir_config.read.strip)
-        else
-          Path.setup('workflows').find(:user)
-        end
-      end
-  end
-
-  def workflow_repo
-    @workflow_repo || 
-      ENV["RBBT_WORKFLOW_REPO"] || 
-      begin 
-        workflow_repo_config = Path.setup("etc/workflow_repo")
-        if workflow_repo_config.exists?
-          workflow_repo_config.read.strip
-        else
-          'https://github.com/Rbbt-Workflows/'
-        end
-      end
-  end
-end
--- lib/rbbt/workflow/refactor.rb
+++ lib/rbbt/workflow/refactor.rb
@@ -106,3 +106,33 @@ module Workflow
     end
   end
 end
+
+module Workflow
+  class << self
+    def workflow_dir
+      @workflow_dir || 
+        ENV["RBBT_WORKFLOW_DIR"] || 
+        begin 
+          workflow_dir_config = Path.setup("etc/workflow_dir")
+          if workflow_dir_config.exists?
+            Path.setup(workflow_dir_config.read.strip)
+          else
+            Path.setup('workflows').find(:user)
+          end
+        end
+    end
+
+    def workflow_repo
+      @workflow_repo || 
+        ENV["RBBT_WORKFLOW_REPO"] || 
+        begin 
+          workflow_repo_config = Path.setup("etc/workflow_repo")
+          if workflow_repo_config.exists?
+            workflow_repo_config.read.strip
+          else
+            'https://github.com/Rbbt-Workflows/'
+          end
+        end
+    end
+  end
+end
